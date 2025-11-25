b8aaa349be241333a349ff3a623bc03455962008
SlockAhuja <ahujaslock321@gmail.com>
2025-11-25 19:51:50 +0530

    Add files via upload

---
+++ Lab 28/lab 28 01.py
@@ -0,0 +1,23 @@
+# Ahuja Slock
+from plotnine import *
+from plotnine.data import mtcars
+
+print(mtcars.head())
+
+
+(
+    ggplot(data=mtcars)
+    + geom_point(mapping=aes(x="wt", y="mpg", color="factor(gear)"))
+    + facet_wrap("~gear")
+)
+
+(ggplot(data=mtcars)
+ + geom_point(aes("wt", "mpg", color="factor(gear)"))
+)
+
+(ggplot(data=mtcars)
+ + geom_point(aes("wt", "mpg", size="factor(gear)"))
+)
+(ggplot(data=mtcars)
+ + geom_point(aes("wt", "mpg"), color='red')
+)
